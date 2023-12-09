305e88d1535c997704fc2b51f4338f456cda2382
Madhav Thapa <madhavth77@gmail.com>
2023-12-08 19:32:43 -0800

    init required architecture folders

--- app/src/main/java/com/madhav/culinaryfood/features/login/presentation/page/MainActivity.kt
+++ app/src/main/java/com/madhav/culinaryfood/features/login/presentation/page/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.madhav.culinaryfood
+package com.madhav.culinaryfood.features.login.presentation.page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.madhav.culinaryfood.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
