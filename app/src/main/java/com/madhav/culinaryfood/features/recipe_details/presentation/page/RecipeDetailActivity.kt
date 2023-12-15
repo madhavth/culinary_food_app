bb942d803f04e16a2299bf6c348a4a42e0da4003
Madhav Thapa <madhavth77@gmail.com>
2023-12-14 23:11:18 -0800

    details page layout done

--- app/src/main/java/com/madhav/culinaryfood/features/recipe_details/presentation/page/RecipeDetailActivity.kt
+++ app/src/main/java/com/madhav/culinaryfood/features/recipe_details/presentation/page/RecipeDetailActivity.kt
@@ -1,4 +1,4 @@
-package com.madhav.culinaryfood.features.register.presentation.page
+package com.madhav.culinaryfood.features.recipe_details.presentation.page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,14 +6,14 @@ import android.os.PersistableBundle
 import com.madhav.culinaryfood.R
 import com.madhav.culinaryfood.core.presentation.page.BaseActivity
 import com.madhav.culinaryfood.databinding.ActivityRecipeDetailBinding
-import com.madhav.culinaryfood.databinding.ActivityRegisterBinding
 
-class RegisterActivity : BaseActivity<ActivityRecipeDetailBinding>() {
+class RecipeDetailActivity : BaseActivity<ActivityRecipeDetailBinding>() {
     override fun postOnCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-
+        TODO("Not yet implemented")
     }
 
     override fun setBinding(): ActivityRecipeDetailBinding {
         return ActivityRecipeDetailBinding.inflate(layoutInflater)
     }
+
 }
