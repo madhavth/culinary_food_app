74fae812a5f1b897f12f0f996fe3440b28e22370
Madhav Thapa <madhavth77@gmail.com>
2023-12-15 23:42:51 -0800

    completed blog

--- app/src/main/java/com/madhav/culinaryfood/features/blog/data/BlogRecyclerAdapter.kt
+++ app/src/main/java/com/madhav/culinaryfood/features/blog/data/BlogRecyclerAdapter.kt
@@ -19,6 +19,7 @@ class BlogRecyclerAdapter(private val myList: List<BlogModel>): ListAdapter<Blog
             binding.btnShare.setOnClickListener {
                 ShareDataHelper().sharePlainText(binding.root.context, blog.title + "\n" + blog.description)
             }
+            binding.tvAuthorName.text = blog.authorId
         }
     }
 
